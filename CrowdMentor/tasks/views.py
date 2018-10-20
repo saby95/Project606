a8963a8f4157323fcd46a318f76b6b12ce1861c6
Diptanil Chaudhuri <diptanil@tamu.edu>
2018-10-19 21:34:30 -0500

    Added view open tasks on worker home page

--- CrowdMentor/tasks/views.py
+++ CrowdMentor/tasks/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from .models import ResearchTasks
 
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    task_list = ResearchTasks.objects.order_by('-creation_time')
+    context = {
+        'task_list': task_list,
+    }
+    return render(request, 'tasks/index.html', context)
+
+@login_required
+def detail(request, task_id):
+    try:
+        task = ResearchTasks.objects.get(pk=task_id)
+    except Question.DoesNotExist:
+        raise Http404("Task does not exist")
+    return render(request, 'tasks/detail.html', {'task': task})
