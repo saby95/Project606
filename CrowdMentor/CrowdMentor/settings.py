3bc05f7e27dcbc268b774fb8e80d4d93bc68500a
abhilash <21965736+vorzawk@users.noreply.github.com>
2018-10-21 08:12:02 -0500

    behave_django installed and feature files added

--- CrowdMentor/CrowdMentor/settings.py
+++ CrowdMentor/CrowdMentor/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '823017eb886440b68e431a9d07031dc7.vfs
 # Application definition
 
 INSTALLED_APPS = [
+    'behave_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'CrowdMentor',
 ]
 
---
+++ CrowdMentor/features/environment.py
@@ -0,0 +1,21 @@
+"""
+behave environment module for testing behave-django
+"""
+
+
+def before_feature(context, feature):
+    if feature.name == 'Fixture loading':
+        context.fixtures = ['behave-fixtures.json']
+
+
+def before_scenario(context, scenario):
+    if scenario.name == 'Load fixtures for this scenario and feature':
+        context.fixtures.append('behave-second-fixture.json')
+
+    if scenario.name == 'Load fixtures then reset sequences':
+        context.fixtures.append('behave-second-fixture.json')
+        context.reset_sequences = True
+
+
+def django_ready(context):
+    context.django = True
