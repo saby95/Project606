9dc1a67afd0c42d541b2008c4bf0728503499a78
abhilash <21965736+vorzawk@users.noreply.github.com>
2018-10-21 15:42:42 -0500

    unit test cases added, coverage is good but testcases are failing

--- CrowdMentor/tasks/tests.py
+++ CrowdMentor/tasks/tests.py
@@ -2,5 +2,32 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+# All the testcases below seem to be failing and I can't figure out why.
+class TasksTest(TestCase):
+    def test_index_view(self):
+        url = reverse('index')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_add_tasks(self):
+        url = reverse('add_tasks')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_detail(self):
+        url = reverse('detail')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_claim(self):
+        url = reverse('index')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_claimed_tasks(self):
+        url = reverse('claimed_tasks')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
