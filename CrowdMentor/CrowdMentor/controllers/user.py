392f1497e19c4c4826ebbee55978571aa71523a6
Sambartika Guha <sambartika.guha@tamu.edu>
2018-10-21 02:15:05 -0500

    Changes for role addition UI added

--- CrowdMentor/CrowdMentor/controllers/user.py
+++ CrowdMentor/CrowdMentor/controllers/user.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from CrowdMentor.models import Profile
+from CrowdMentor.utilities.changeRolesForm import ChangeRolesForm
 
 dict_roles= {'UserRoles.WORKER': 'worker', 'UserRoles.TASK_UPDATER': 'task_updater', 'UserRoles.AUDITOR': 'auditor',
              'UserRoles.ADMIN': 'admin', 'UserRoles.MENTOR': 'mentor'}
@@ -20,4 +21,10 @@ def view(request):
 
 @login_required
 def change_roles(request):
-    return render(request, 'changeRoles.html')
+    users= User.objects.all()
+    user_dict=dict()
+    for usr in users:
+        prf = Profile.objects.get(user_id=usr.id)
+        user_dict[usr.id] = [usr.username, usr.email, prf.role]
+    form = ChangeRolesForm(request.POST, users=user_dict)
+    return render(request, 'changeRoles.html', {'form': form})
