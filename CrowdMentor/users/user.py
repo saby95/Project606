f08f8b16e3227a9b28e16001a320568c03e4ee6e
Sambartika Guha <sambartika.guha@tamu.edu>
2018-11-04 01:16:01 -0500

    Permissions added and home view corrected based on user role

--- CrowdMentor/users/user.py
+++ CrowdMentor/users/user.py
@@ -4,21 +4,38 @@ from django.contrib.auth.models import User
 from profile import Profile
 from UserRoles import UserRoles
 from changeRolesForm import ChangeRolesForm
-
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 @login_required
 def view(request):
     user_id = User.objects.get(username=request.user.username).id
     profile = Profile.objects.get(user_id=user_id).role
-    dict_functs={'/tasks/': 'View open tasks', '/tasks/claimed': 'View claimed tasks'}
-    if profile == UserRoles.TASK_UPDATER.value or profile == UserRoles.ADMIN.value:
+    dict_functs={}
+    if profile == UserRoles.TASK_UPDATER.value:
         dict_functs['/tasks/add_tasks']= 'Add task'
+
+    if profile == UserRoles.ADMIN.value:
         dict_functs['/change_roles'] = 'Change user roles'
+
+    if profile == UserRoles.WORKER.value:
+        dict_functs['/tasks/claimed/'] = 'View claimed tasks'
+        dict_functs['/tasks/'] = 'View open tasks'
+
+    if profile == UserRoles.AUDITOR.value:
+        dict_functs['/tasks/open_audits/'] = 'View open audits'
+        dict_functs['/tasks/audits/'] = 'View claimed audits'
 
     return render(request, 'home.html', {"profile": profile, "dict_functs" : dict_functs})
 
-
+@login_required
 def change_roles(request):
+    user = User.objects.get(username=request.user.username)
+    profile = user.profile.role
+    if profile != 'admin':
+        messages.info(request, 'Permission Denied!! You do not have permission to access this page')
+        return HttpResponseRedirect('/')
+
     users = User.objects.all()
     if request.method == 'POST':
         for user in users:
