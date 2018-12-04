82dfab18e0f03488e2c2ccbd1b5f50eece179a37
Diptanil Chaudhuri <diptanil@tamu.edu>
2018-12-03 21:21:49 -0600

    Deployed to heroku

--- CrowdMentor/users/settings.py
+++ CrowdMentor/users/settings.py
@@ -152,11 +152,11 @@ LOGIN_REDIRECT_URL = 'home'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #For production. Uncomment while deployment
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'diptanil'
-EMAIL_HOST_PASSWORD = 'Abcd1234'
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'CrowdMentor Team <noreply@mirc-crowdmentor.com>'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
